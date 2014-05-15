3316ce5c511ce917618575f602900094697f209a
plondon <plondon@gm.slc.edu>
2014-05-15 15:20:31 -0400

    add passwords to users table

--- SoundCrowd/db/migrate/20140515190958_create_users_table.rb
+++ SoundCrowd/db/migrate/20140515190958_create_users_table.rb
@@ -2,6 +2,7 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
+      t.string :password
       t.integer :total_votes
       t.timestamps
     end
