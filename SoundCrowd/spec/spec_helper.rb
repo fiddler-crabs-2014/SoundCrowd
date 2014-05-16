7b56b2e2c25721fef64b6742e9398f3614845fa2
phil london & Shaun McGeever <pair+plondon.shaunmcgeever@devbootcamp.com>
2014-05-16 00:52:28 -0400

    added test coverage stats

--- SoundCrowd/spec/spec_helper.rb
+++ SoundCrowd/spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'rspec/rails'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+require 'simplecov'
+SimpleCov.start
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
